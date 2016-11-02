9f2bf8be5f6004a1843004f4e474be739163635c
icyfillup <phil10tran@gmail.com>
2016-11-01 17:41:55 -0700

    no comment

--- file/nodeprojects/contactlistapp/server.js
+++ file/nodeprojects/contactlistapp/server.js
@@ -1,36 +1,29 @@
+
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
-app.get('/contactlist', function(request, response)
-	{
-	    console.log("I received a GET request")
+app.get('/contactlist', function (req, res) {
+    console.log('I received a GET request');
 
-	    person1 = 
-		{
-		    name: 'tim',
-		    email: 'time@email.com',
-		    number: '(111) 111-1111'
-		};
+    db.contactlist.find(function (err, docs) {
+	console.log(docs);
+	res.json(docs);
+    });
+});
 
-	    person2 = 
-		{
-		    name: 'tom',
-		    email: 'tom@email.com',
-		    number: '(222) 111-1111'
-		};
-
-	    person3 = 
-		{
-		    name: 'john',
-		    email: 'john@email.com',
-		    number: '(333) 111-1111'
-		};
-
-	    var contactlist = [person1, person2, person3];
-	    response.json(contactlist)
-	})
+app.post("/contactlist", function(request, response){
+    console.log(request.body);
+    db.contactlist.insert(request.body, function(error, docs)
+			{
+			    response.json(docs);
+			});
+});
 
 app.listen(3000);
 console.log("Server running on port 3000");
